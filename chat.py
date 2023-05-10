8f9642fd0a1542a27a262cc986a5318d9ec0e7c4
avivlyweb <73378473+avivlyweb@users.noreply.github.com>
2023-05-10 14:36:55 +0200

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,3 +1,4 @@
+
 import os
 
 import replicate
@@ -16,52 +17,58 @@ eleven_api_key = os.getenv("ELEVEN_API_KEY")
 llm = OpenAI(temperature=0.9)
 
 def generate_story(text):
+    """Generate a story using the langchain library and OpenAI's GPT-3 model."""
     prompt = PromptTemplate(
         input_variables=["text"],
         template=""" 
-        ...
-        """,
-        text=text  # Add this line to provide the missing "text" parameter
+         You are a fun and seasoned storyteller. Generate a story for me about {text}.
+                 """
     )
     story = LLMChain(llm=llm, prompt=prompt)
     return story.run(text=text)
 
+
 def generate_audio(text, voice):
+    """Convert the generated story to audio using the Eleven Labs API."""
     audio = generate(text=text, voice=voice, api_key=eleven_api_key)
     return audio
 
+
 def generate_images(story_text):
+    """Generate images using the story text using the Replicate API."""
     output = replicate.run(
         "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
         input={"prompt": story_text}
     )
     return output
 
+
 def app():
-    st.title("Physiotherapy Case Study Generator")
+    st.title("Story Storm")
 
     with st.form(key='my_form'):
         text = st.text_input(
-            "Enter a word related to physiotherapy to generate a case study",
+            "Enter a word to generate a story",
             max_chars=None,
             type="default",
-            placeholder="Enter a word related to physiotherapy to generate a case study",
+            placeholder="Enter a word to generate a story",
         )
-        options = ["Brian", "Clara", "David", "Emily", "Julia", "Kevin", "Laura", "Maggie", "Thomas"]
+        options = ["Bella", "Antoni", "Arnold", "Adam", "Domi", "Elli", "Josh", "Rachel", "Sam"]
         voice = st.selectbox("Select a voice", options)
 
         if st.form_submit_button("Submit"):
-            with st.spinner('Generating case study...'):
-                case_study_text = generate_story(text)
-                audio = generate_audio(case_study_text, voice)
+            with st.spinner('Generating story...'):
+                story_text = generate_story(text)
+                audio = generate_audio(story_text, voice)
 
             st.audio(audio, format='audio/mp3')
-            images = generate_images(case_study_text)
+            images = generate_images(story_text)
             for item in images:
                 st.image(item)
 
     if not text or not voice:
-        st.info("Please enter a word related to physiotherapy and select a voice")
+        st.info("Please enter a word and select a voice")
+
 
 if __name__ == '__main__':
     app()
